1147f8de00a68bd84249232b4691e45dfc181942
swapnilamore <yogesh.amore@gmail.com>
2020-11-17 00:49:54 +0530

    Added project & Blog links. Work under progress

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -41,13 +41,22 @@ export default class Sidebar extends Component {
                       About
                     </a>
                   </li>
-                  {/*<li><a href="#" data-nav-section="projects">Projects</a></li>
-                  <li><a href="#" data-nav-section="blog">Blog</a></li>*/}
                   <li>
                     <a href="#timeline" data-nav-section="timeline">
                       Timeline
                     </a>
                   </li>
+                  <li>
+                    <a href="#projects" data-nav-section="projects">
+                      Projects
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" data-nav-section="blog">
+                      Blog
+                    </a>
+                  </li>
+                 
                 </ul>
               </div>
             </nav>
